open-craft/openedx-certificates

feat: implement Certificate XBlock

openedx_certificates/models.py
@@ -162,10 +162,11 @@ class ExternalCertificateCourseConfiguration(TimeStampedModel):
         filtered_user_ids_set = set(user_ids) - set(users_ids_with_certificates)
         return list(filtered_user_ids_set)
 
-    def get_eligible_user_ids(self) -> list[int]:
+    def get_eligible_user_ids(self, user_id: int = None) -> list[int]:
         """
         Get the list of eligible learners for the given course.
 
+        :param user_id: Optional. If provided, will check eligibility for the specific user.
         :return: A list of user IDs.
         """
         func_path = self.certificate_type.retrieval_func
@@ -174,7 +175,7 @@ class ExternalCertificateCourseConfiguration(TimeStampedModel):
         func = getattr(module, func_name)
 
         custom_options = {**self.certificate_type.custom_options, **self.custom_options}
-        return func(self.course_id, custom_options)
+        return func(self.course_id, custom_options, user_id=user_id)
 
     def generate_certificate_for_user(self, user_id: int, celery_task_id: int = 0):
         """

openedx_certificates/xblocks.py
@@ -1,0 +1,102 @@
+from __future__ import annotations
+
+import logging
+
+from xblock.core import XBlock
+from xblock.fields import Scope, String
+from xblock.fragment import Fragment
+
+try:
+    from xblock.utils.resources import ResourceLoader
+    from xblock.utils.settings import ThemableXBlockMixin, XBlockWithSettingsMixin
+    from xblock.utils.studio_editable import StudioEditableXBlockMixin
+except ModuleNotFoundError:  # For backward compatibility with releases older than Quince.
+    from xblockutils.resources import ResourceLoader
+    from xblockutils.settings import ThemableXBlockMixin, XBlockWithSettingsMixin
+    from xblockutils.studio_editable import StudioEditableXBlockMixin
+
+from .api import generate_certificate_for_user, get_eligible_users_by_certificate_type, get_user_certificates_by_type
+
+loader = ResourceLoader(__name__)
+logger = logging.getLogger(__name__)
+
+
+class CertificatesXBlock(StudioEditableXBlockMixin, XBlock):
+    """
+    XBlock that displays the certificate eligibility status and allows eligible users to generate certificates.
+    """
+
+    display_name = String(
+        help='The display name for this component.',
+        scope=Scope.content,
+        display_name="Display name",
+        default='Certificates',
+    )
+
+    def student_view(self, context) -> Fragment:
+        """
+        Main view for the student. Displays the certificate eligibility or ineligibility status.
+        """
+        fragment = Fragment()
+        eligible_types = False
+        certificates = []
+
+        if not (is_author_mode := getattr(self.runtime, 'is_author_mode', False)):
+            certificates = self.get_certificates()
+            eligible_types = self.get_eligible_certificate_types()
+
+            # Filter out the eligible types that already have a certificate generated
+            for cert_type in certificates.keys():
+                if cert_type in eligible_types:
+                    del eligible_types[cert_type]
+
+        fragment.add_content(
+            loader.render_django_template(
+                'public/html/certificates_xblock.html',
+                {
+                    'certificates': certificates,
+                    'eligible_types': eligible_types,
+                    'is_author_mode': is_author_mode,
+                },
+            )
+        )
+
+        fragment.add_css_url(self.runtime.local_resource_url(self, "public/css/certificates_xblock.css"))
+        fragment.add_javascript_url(self.runtime.local_resource_url(self, "public/js/certificates_xblock.js"))
+        fragment.initialize_js('CertificatesXBlock')
+        return fragment
+
+    def get_eligible_certificate_types(self) -> dict[str, bool]:
+        """
+        Retrieve the eligibility status for each certificate type.
+        """
+        eligible_users = get_eligible_users_by_certificate_type(self.runtime.course_id, user_id=self.scope_ids.user_id)
+
+        return {certificate_type: bool(users) for certificate_type, users in eligible_users.items()}
+
+    def get_certificates(self) -> dict[str, dict[str, str]]:
+        """
+        Retrieve the certificates for the current user in the current course.
+        """
+        return get_user_certificates_by_type(self.runtime.course_id, self.scope_ids.user_id)
+
+    @XBlock.json_handler
+    def generate_certificate(self, data, suffix='') -> dict[str, str]:
+        """
+        Handler for generating a certificate for a specific type.
+        """
+        certificate_type = data.get('certificate_type')
+        if not certificate_type:
+            return {'status': 'error', 'message': 'No certificate type specified.'}
+
+        course_id = self.runtime.course_id
+        user_id = self.scope_ids.user_id
+        logger.info(
+            'Generating a certificate for user %s in course %s with type %s.', user_id, course_id, certificate_type
+        )
+
+        try:
+            generate_certificate_for_user(course_id, certificate_type, user_id)
+        except ValueError as e:
+            return {'status': 'error', 'message': str(e)}
+        return {'status': 'success'}

tests/test_models.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
     from opaque_keys.edx.keys import CourseKey
 
 
-def _mock_retrieval_func(_course_id: CourseKey, _options: dict[str, Any]) -> list[int]:
+def _mock_retrieval_func(_course_id: CourseKey, _options: dict[str, Any], user_id: int = None) -> list[int]:
     return [1, 2, 3]
 
 

open-craft/learning-credentials

learning_credentials/models.py
@@ -165,10 +165,11 @@ class CredentialConfiguration(TimeStampedModel):
         filtered_user_ids_set = set(user_ids) - set(users_ids_with_credentials)
         return list(filtered_user_ids_set)
 
-    def get_eligible_user_ids(self) -> list[int]:
+    def get_eligible_user_ids(self, user_id: int = None) -> list[int]:
         """
         Get the list of eligible learners for the given course.
 
+        :param user_id: Optional. If provided, will check eligibility for the specific user.
         :return: A list of user IDs.
         """
         func_path = self.credential_type.retrieval_func
@@ -177,7 +178,7 @@ class CredentialConfiguration(TimeStampedModel):
         func = getattr(module, func_name)
 
         custom_options = {**self.credential_type.custom_options, **self.custom_options}
-        return func(self.learning_context_key, custom_options)
+        return func(self.learning_context_key, custom_options, user_id=user_id)
 
     def generate_credential_for_user(self, user_id: int, celery_task_id: int = 0):
         """

learning_credentials/xblocks.py
@@ -1,0 +1,102 @@
+from __future__ import annotations
+
+import logging
+
+from xblock.core import XBlock
+from xblock.fields import Scope, String
+from xblock.fragment import Fragment
+
+try:
+    from xblock.utils.resources import ResourceLoader
+    from xblock.utils.settings import ThemableXBlockMixin, XBlockWithSettingsMixin
+    from xblock.utils.studio_editable import StudioEditableXBlockMixin
+except ModuleNotFoundError:  # For backward compatibility with releases older than Quince.
+    from xblockutils.resources import ResourceLoader
+    from xblockutils.settings import ThemableXBlockMixin, XBlockWithSettingsMixin
+    from xblockutils.studio_editable import StudioEditableXBlockMixin
+
+from .api import generate_credential_for_user, get_eligible_users_by_credential_type, get_user_credentials_by_type
+
+loader = ResourceLoader(__name__)
+logger = logging.getLogger(__name__)
+
+
+class CredentialsXBlock(StudioEditableXBlockMixin, XBlock):
+    """
+    XBlock that displays the credential eligibility status and allows eligible users to generate credentials.
+    """
+
+    display_name = String(
+        help='The display name for this component.',
+        scope=Scope.content,
+        display_name="Display name",
+        default='Credentials',
+    )
+
+    def student_view(self, context) -> Fragment:
+        """
+        Main view for the student. Displays the credential eligibility or ineligibility status.
+        """
+        fragment = Fragment()
+        eligible_types = False
+        credentials = []
+
+        if not (is_author_mode := getattr(self.runtime, 'is_author_mode', False)):
+            credentials = self.get_credentials()
+            eligible_types = self.get_eligible_credential_types()
+
+            # Filter out the eligible types that already have a credential generated
+            for cred_type in credentials.keys():
+                if cred_type in eligible_types:
+                    del eligible_types[cred_type]
+
+        fragment.add_content(
+            loader.render_django_template(
+                'public/html/credentials_xblock.html',
+                {
+                    'credentials': credentials,
+                    'eligible_types': eligible_types,
+                    'is_author_mode': is_author_mode,
+                },
+            )
+        )
+
+        fragment.add_css_url(self.runtime.local_resource_url(self, "public/css/credentials_xblock.css"))
+        fragment.add_javascript_url(self.runtime.local_resource_url(self, "public/js/credentials_xblock.js"))
+        fragment.initialize_js('CredentialsXBlock')
+        return fragment
+
+    def get_eligible_credential_types(self) -> dict[str, bool]:
+        """
+        Retrieve the eligibility status for each credential type.
+        """
+        eligible_users = get_eligible_users_by_credential_type(self.runtime.course_id, user_id=self.scope_ids.user_id)
+
+        return {credential_type: bool(users) for credential_type, users in eligible_users.items()}
+
+    def get_credentials(self) -> dict[str, dict[str, str]]:
+        """
+        Retrieve the credentials for the current user in the current course.
+        """
+        return get_user_credentials_by_type(self.runtime.course_id, self.scope_ids.user_id)
+
+    @XBlock.json_handler
+    def generate_credential(self, data, suffix='') -> dict[str, str]:
+        """
+        Handler for generating a credential for a specific type.
+        """
+        credential_type = data.get('credential_type')
+        if not credential_type:
+            return {'status': 'error', 'message': 'No credential type specified.'}
+
+        course_id = self.runtime.course_id
+        user_id = self.scope_ids.user_id
+        logger.info(
+            'Generating a credential for user %s in course %s with type %s.', user_id, course_id, credential_type
+        )
+
+        try:
+            generate_credential_for_user(course_id, credential_type, user_id)
+        except ValueError as e:
+            return {'status': 'error', 'message': str(e)}
+        return {'status': 'success'}

tests/test_models.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
     from opaque_keys.edx.keys import LearningContextKey
 
 
-def _mock_retrieval_func(_context_id: LearningContextKey, _options: dict[str, Any]) -> list[int]:
+def _mock_retrieval_func(_context_id: LearningContextKey, _options: dict[str, Any], user_id: int = None) -> list[int]:
     return [1, 2, 3]
 
 